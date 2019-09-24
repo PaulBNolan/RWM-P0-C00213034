d9d90b26c8e4cf078b557cf0dbdbbaf30d3de4d5
PaulBNolan <C00213034@itcarlow.ie>
2019-09-24 20:32:44 +0100

    Function and test implemented

---
+++ RWM-P0-C00213034/Assets/Scripts/CubeFilter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFilter
+{
+    public static int collatz(int n)
+    {
+        if(n % 2 == 0)
+        {
+            n = n / 2;
+        }
+        else
+        {
+            n = n * 3 - 1;
+        }
+        return n;
+    }
+
+    public static int [] cubes(int [] nums)
+    {
+        int[] res = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            res[i] = collatz(nums[i]);
+        }
+
+        return res;
+    }
+}
---
+++ RWM-P0-C00213034/Assets/Tests/CubeFilterTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CubeFilterTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void CubeFilterTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [Test]
+        public void collatzTest()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] target = new int[] { 2, 1, 8, 2, 14, 3, 20, 4, 26 };
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            array = CubeFilter.cubes(array);
+            
+
+            Assert.AreEqual(array, target);
+        }
+    }
+}
